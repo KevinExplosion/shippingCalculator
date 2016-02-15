fd2a5c6af660244316f83f3117d274097c0621ad
kevin mattison and tyler suderman <mattisonii@yahoo.com>
2016-02-15 13:23:12 -0800

    create specs for parcel arguments

--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -9,9 +9,27 @@ public class ParcelTest {
     assertEquals(true, testParcel instanceof Parcel);
   }
 
-  // @Test
-  // public void newParcel_setsHeight() {
-  //   Parcel testParcel = new Parcel(1, 1, 1, 4);
-  //   assertEquals(1, testParcel.getHeight());
-  // }
+  @Test
+  public void newParcel_setsHeight() {
+    Parcel testParcel = new Parcel(1, 1, 1, 4);
+    assertEquals(1, testParcel.getHeight());
+  }
+
+  @Test
+  public void newParcel_setsWidth() {
+    Parcel testParcel = new Parcel(1, 1, 1, 4);
+    assertEquals(1, testParcel.getWidth());
+  }
+
+  @Test
+  public void newParcel_setsLength() {
+    Parcel testParcel = new Parcel(1, 1, 1, 4);
+    assertEquals(1, testParcel.getLength());
+  }
+
+  @Test
+  public void newParcel_setsWeight() {
+    Parcel testParcel = new Parcel(1, 1, 1, 4);
+    assertEquals(4, testParcel.getWeight());
+  }
 }
